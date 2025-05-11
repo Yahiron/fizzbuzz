1cacd12ea1c55901b7e36af8507ec960db3436e1
Yahiron <j0821305@yahoo.co.jp>
2025-05-11 21:03:07 +0900

    serversのリファクタリング

--- src/main/java/com/example/springtutorial/service/FizzbuzzService.java
+++ src/main/java/com/example/springtutorial/service/FizzbuzzService.java
@@ -8,9 +8,11 @@ public class FizzbuzzService {
 	public String fizzbuzz(int i) {
 		if (i % 15 == 0) {
 			return "fizzbuzz";
-		} else if (i % 3 == 0) {
+		}
+		if (i % 3 == 0) {
 			return "fizz";
-		} else if (i % 5 == 0) {
+		}
+		if (i % 5 == 0) {
 			return "buzz";
 		}
 		return String.valueOf(i);
